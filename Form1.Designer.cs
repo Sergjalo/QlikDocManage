6c6678ac78163b5e498a7f86ef83d04aca159d64
Sergjalo <Sergjalo@gmail.com>
2016-06-27 16:10:09 +0300

    firstCommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.dOF = new System.Windows.Forms.OpenFileDialog();
+            this.lCnt = new System.Windows.Forms.Label();
+            this.dgVars = new System.Windows.Forms.DataGridView();
+            this.txValCorrect = new System.Windows.Forms.TextBox();
+            this.txCommCorrect = new System.Windows.Forms.TextBox();
+            this.txFlName = new System.Windows.Forms.TextBox();
+            this.bSaveChg = new System.Windows.Forms.Button();
+            this.col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.col4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bExport = new System.Windows.Forms.Button();
+            this.bLoad = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txFlNameComm = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgVars)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get QV doc";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dOF
+            // 
+            this.dOF.FileName = "FleetCompCode.qvw";
+            this.dOF.Filter = "QlikView|*.qvw|Excel|*.xls*";
+            this.dOF.FilterIndex = 2;
+            this.dOF.InitialDirectory = "d:\\work\\PNSK-CER\\";
+            // 
+            // lCnt
+            // 
+            this.lCnt.AutoSize = true;
+            this.lCnt.Location = new System.Drawing.Point(12, 64);
+            this.lCnt.Name = "lCnt";
+            this.lCnt.Size = new System.Drawing.Size(0, 13);
+            this.lCnt.TabIndex = 1;
+            // 
+            // dgVars
+            // 
+            this.dgVars.AllowUserToOrderColumns = true;
+            this.dgVars.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgVars.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgVars.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.col1,
+            this.col2,
+            this.col3,
+            this.col4});
+            this.dgVars.Location = new System.Drawing.Point(15, 202);
+            this.dgVars.Name = "dgVars";
+            this.dgVars.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgVars.ShowCellToolTips = false;
+            this.dgVars.Size = new System.Drawing.Size(1047, 349);
+            this.dgVars.TabIndex = 3;
+            this.dgVars.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgVars_CellContentClick);
+            this.dgVars.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgVars_RowEnter);
+            this.dgVars.RowLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgVars_RowLeave);
+            this.dgVars.DoubleClick += new System.EventHandler(this.dgVars_DoubleClick);
+            // 
+            // txValCorrect
+            // 
+            this.txValCorrect.Location = new System.Drawing.Point(15, 96);
+            this.txValCorrect.Multiline = true;
+            this.txValCorrect.Name = "txValCorrect";
+            this.txValCorrect.ReadOnly = true;
+            this.txValCorrect.Size = new System.Drawing.Size(413, 100);
+            this.txValCorrect.TabIndex = 4;
+            // 
+            // txCommCorrect
+            // 
+            this.txCommCorrect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txCommCorrect.Location = new System.Drawing.Point(434, 96);
+            this.txCommCorrect.Multiline = true;
+            this.txCommCorrect.Name = "txCommCorrect";
+            this.txCommCorrect.Size = new System.Drawing.Size(628, 100);
+            this.txCommCorrect.TabIndex = 4;
+            this.txCommCorrect.Leave += new System.EventHandler(this.txCommCorrect_Leave);
+            // 
+            // txFlName
+            // 
+            this.txFlName.Location = new System.Drawing.Point(111, 15);
+            this.txFlName.Name = "txFlName";
+            this.txFlName.Size = new System.Drawing.Size(508, 20);
+            this.txFlName.TabIndex = 5;
+            this.txFlName.Text = "d:\\work\\PNSK-CER\\FleetCompCode.qvw";
+            // 
+            // bSaveChg
+            // 
+            this.bSaveChg.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bSaveChg.Location = new System.Drawing.Point(12, 44);
+            this.bSaveChg.Name = "bSaveChg";
+            this.bSaveChg.Size = new System.Drawing.Size(93, 23);
+            this.bSaveChg.TabIndex = 6;
+            this.bSaveChg.Text = "Save changes";
+            this.bSaveChg.UseVisualStyleBackColor = true;
+            this.bSaveChg.Click += new System.EventHandler(this.bSaveChg_Click);
+            // 
+            // col1
+            // 
+            this.col1.HeaderText = "VarName";
+            this.col1.Name = "col1";
+            this.col1.ReadOnly = true;
+            // 
+            // col2
+            // 
+            this.col2.DividerWidth = 1;
+            this.col2.HeaderText = "Value";
+            this.col2.Name = "col2";
+            this.col2.ReadOnly = true;
+            this.col2.Width = 600;
+            // 
+            // col3
+            // 
+            this.col3.HeaderText = "Comment";
+            this.col3.Name = "col3";
+            this.col3.ReadOnly = true;
+            this.col3.Width = 500;
+            // 
+            // col4
+            // 
+            this.col4.HeaderText = "VarIndex";
+            this.col4.Name = "col4";
+            this.col4.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Edit value:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(444, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Edit comment:";
+            // 
+            // bExport
+            // 
+            this.bExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bExport.Location = new System.Drawing.Point(19, 21);
+            this.bExport.Name = "bExport";
+            this.bExport.Size = new System.Drawing.Size(93, 23);
+            this.bExport.TabIndex = 6;
+            this.bExport.Text = "Export to xls";
+            this.bExport.UseVisualStyleBackColor = true;
+            this.bExport.Click += new System.EventHandler(this.bExport_Click);
+            // 
+            // bLoad
+            // 
+            this.bLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bLoad.Location = new System.Drawing.Point(140, 21);
+            this.bLoad.Name = "bLoad";
+            this.bLoad.Size = new System.Drawing.Size(93, 23);
+            this.bLoad.TabIndex = 6;
+            this.bLoad.Text = "Load from xls";
+            this.bLoad.UseVisualStyleBackColor = true;
+            this.bLoad.Click += new System.EventHandler(this.bLoad_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.bLoad);
+            this.groupBox1.Controls.Add(this.bExport);
+            this.groupBox1.Controls.Add(this.txFlNameComm);
+            this.groupBox1.Location = new System.Drawing.Point(625, 11);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(437, 79);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Comments";
+            // 
+            // txFlNameComm
+            // 
+            this.txFlNameComm.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txFlNameComm.Location = new System.Drawing.Point(19, 53);
+            this.txFlNameComm.Name = "txFlNameComm";
+            this.txFlNameComm.Size = new System.Drawing.Size(411, 20);
+            this.txFlNameComm.TabIndex = 5;
+            this.txFlNameComm.Text = "d:\\work\\PNSK-CER\\VariablesMain.xls";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 563);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bSaveChg);
+            this.Controls.Add(this.txFlName);
+            this.Controls.Add(this.txCommCorrect);
+            this.Controls.Add(this.txValCorrect);
+            this.Controls.Add(this.dgVars);
+            this.Controls.Add(this.lCnt);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Variables from Qvw";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgVars)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog dOF;
+        private System.Windows.Forms.Label lCnt;
+        private System.Windows.Forms.DataGridView dgVars;
+        private System.Windows.Forms.TextBox txValCorrect;
+        private System.Windows.Forms.TextBox txCommCorrect;
+        private System.Windows.Forms.TextBox txFlName;
+        private System.Windows.Forms.Button bSaveChg;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bExport;
+        private System.Windows.Forms.Button bLoad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txFlNameComm;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel; 
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        QlikInstance QV;
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class QlikInstance
+        {
+            public dynamic doc;
+            public dynamic v;
+
+            public dynamic Doc
+            {
+                get { return doc; }
+                set { doc = value; }
+            }
+
+            public dynamic V
+            {
+                get { return v; }
+                set { v = value;  }
+            }
+
+            public HashSet<String> varS;
+
+            public QlikInstance (string sFileName)
+            {
+                String[] a = new String[] {"CD","QvPath",	"QvRoot",	"QvWorkPath",	"QvWorkRoot",	"WinPath",	"WinRoot",	"ErrorMode",	"StripComments",
+                "OpenUrlTimeout",	"ScriptErrorCount",	"ScriptErrorList",	
+                "ScriptError",	"ThousandSep",	"DecimalSep",	"MoneyThousandSep",	"MoneyDecimalSep",	"MoneyFormat",	"TimeFormat",	"DateFormat",	
+                "TimestampFormat",	"MonthNames",	"DayNames",	"HidePrefix",	"ScriptErrorDetails", "FLOPPY"};
+                varS = new HashSet<String>(a);
+                System.Type objType = System.Type.GetTypeFromProgID("QlikTech.QlikView");
+                dynamic comObject = System.Activator.CreateInstance(objType);
+                Doc = comObject.OpenDoc(sFileName); 
+                V = Doc.GetVariableDescriptions();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //txVarNames.Text = "";
+            dgVars.Rows.Clear();
+
+
+           dOF.FileName = Path.GetFileName(txFlName.Text);
+           dOF.InitialDirectory = Path.GetDirectoryName(txFlName.Text);
+           dOF.FilterIndex = 1;
+
+            if (dOF.ShowDialog() == DialogResult.OK)
+            {
+                try {
+                   QV.doc.CloseDoc();
+                }
+                catch { }
+                txFlName.Text = dOF.FileName;
+                QV = new QlikInstance(dOF.FileName);
+                lCnt.Text = "Non-system variables count - " + QV.v.count.ToString();
+
+                string[] row0 = {"","","",""};
+                string varname;
+                string VarVal;
+                string comment;
+                for (int i = 0; i < QV.v.count; i++)
+                {
+                    varname = QV.v.Item(i).Name;
+                    if (!QV.varS.Contains(varname))
+                    {
+                        //txVarNames.AppendText(varname);
+                        //txVarNames.AppendText(Environment.NewLine);
+
+                        VarVal = QV.doc.Variables(varname).GetRawContent();
+                        comment = QV.doc.Variables(varname).GetComment();
+                        row0[0] = varname;
+                        row0[1] = VarVal;
+                        row0[2] = comment;
+                        //row0[3] = i.ToString();
+                        dgVars.Rows.Add(row0);
+                    }
+                }
+                //QV.doc.CloseDoc();
+                bSaveChg.Enabled = false;
+            }
+        }
+
+        private void dgVars_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgVars_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+
+            txValCorrect.Text =  (string)dgVars[1, e.RowIndex].Value;
+            txCommCorrect.Text = (string)dgVars[2, e.RowIndex].Value;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgVars_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txCommCorrect_Leave(object sender, EventArgs e)
+        {
+            //string t = (string)dgVars[3, dgVars.CurrentRow.Index].Value;
+            dgVars[2, dgVars.CurrentRow.Index].Value = txCommCorrect.Text;
+            //int t = dgVars.CurrentRow.Index;
+            QV.doc.Variables(dgVars[0, dgVars.CurrentRow.Index].Value).SetComment(txCommCorrect.Text);
+            //string t= QV.doc.Variables(dgVars[0, dgVars.CurrentRow.Index].Value).GetComment();
+            //QV.doc.Save();
+            bSaveChg.Enabled = true;
+        }
+
+        private void bSaveChg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bSaveChg.Enabled = false;
+                for (int i = 0; i < dgVars.Rows.Count-1; i++)
+                {
+                    QV.doc.Variables(dgVars[0, i].Value).SetComment(dgVars[2, i].Value);    
+                }
+                QV.doc.Save();
+            }
+            catch
+            {
+                bSaveChg.Enabled = true;
+                bSaveChg.Text = "Saving error";
+            }
+        }
+
+        private void dgVars_RowLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            /*
+            if (txCommCorrect.Text != (string)dgVars[2, e.RowIndex].Value)
+            {
+                bSaveChg.Enabled = true;
+                txCommCorrect.Text = (string)dgVars[2, e.RowIndex].Value;
+                QV.doc.Variables(dgVars[0, dgVars.CurrentRow.Index].Value).SetComment(txCommCorrect.Text);
+            }
+             */ 
+        }
+
+        private void bExport_Click(object sender, EventArgs e)
+        {
+           dOF.FileName = Path.GetFileName(txFlNameComm.Text);
+           dOF.InitialDirectory = Path.GetDirectoryName(txFlNameComm.Text);
+           dOF.FilterIndex = 2;
+
+
+            if (dOF.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            txFlNameComm.Text = dOF.FileName;
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            int i = 0;
+            int j = 0;
+            
+            for (i = 0; i <= dgVars.RowCount - 1; i++)
+            {
+                for (j = 0; j <= dgVars.ColumnCount - 1; j++)
+                {
+                    DataGridViewCell cell = dgVars[j, i];
+                    xlWorkSheet.Cells[i + 1, j + 1].Value2 = "'"+(string)cell.Value;
+                }
+            }
+
+            xlWorkBook.SaveAs(dOF.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+
+        private void bLoad_Click(object sender, EventArgs e)
+        {
+            dOF.FileName = Path.GetFileName(txFlNameComm.Text);
+            dOF.InitialDirectory = Path.GetDirectoryName(txFlNameComm.Text);
+            dOF.FilterIndex = 2;
+
+            if (dOF.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            txFlNameComm.Text = dOF.FileName;
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+            object misValue = System.Reflection.Missing.Value;
+            
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(
+                               dOF.FileName, 0, true, 5,
+                               "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false,
+                               0, true, 0, 0); 
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            int i = 0;
+            int j = 0;
+            /*
+            for (i = 0; i <= dgVars.RowCount - 1; i++)
+            {
+                for (j = 0; j <= dgVars.ColumnCount - 1; j++)
+                {
+                    dgVars[j, i].Value = (string)xlWorkSheet.Cells[i + 1, j + 1].value2;
+                }
+            }
+             */ 
+            
+            range = xlWorkSheet.UsedRange;
+            /*
+            range = (Excel.Range) xlWorkSheet.Range[xlWorkSheet.Cells[1, 1], xlWorkSheet.Cells[3, 3]];
+            System.Array myvalues = (System.Array)range.Cells.Value;
+            string[] strArray = ConvertToStringArray(myvalues);
+            */
+            string s;
+            for (i = 1; i <= range.Rows.Count; i++)
+            {
+                s = (string)(range.Cells[i, 1] as Excel.Range).Value;
+                for (j = 0; j <= dgVars.Rows.Count - 1; j++)
+                {   // compare names of vars - from 0 col in grid and 1 in xls
+                    if (dgVars[0, j].Value != null && dgVars[0, j].Value.Equals(s))
+                    {
+                        s = (string)(range.Cells[i, 3] as Excel.Range).Value;
+                        if (dgVars[2, j].Value != null && !dgVars[2, j].Value.Equals(s))
+                        {
+                            dgVars[2,j].Value = s;
+                        }
+                        break;
+                    }
+                }
+            }
+            /*
+            for (i = 1; i <= range.Rows.Count; i++)
+            {
+                for (j = 1; j <= range.Columns.Count; j++)
+                {
+                    try
+                    {
+                        dgVars[j-1, i-1].Value = (string)(range.Cells[i, j] as Excel.Range).Value;
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            dgVars[j - 1, i - 1].Value = ((double)(range.Cells[i, j] as Excel.Range).Value).ToString();
+                        }
+                        catch
+                        {
+                            dgVars[j - 1, i - 1].Value = ((DateTime)(range.Cells[i, j] as Excel.Range).Value).ToString("yyyy-MM-dd HH:mm");
+                        }
+                    }
+                    //string s= (string)(range.Cells[i, j] as Excel.Range).Value;
+                }
+            }
+            */
+
+            //xlWorkBook.SaveAs(Path.GetDirectoryName(dOF.FileName) + "\\test.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+            bSaveChg.Enabled = true;
+        }
+        /*
+        string[] ConvertToStringArray(System.Array values)
+        {
+            string[] theArray = new string[values.Length];
+            for (int i = 1; i <= values.Length; i++)
+            {
+                if (values.GetValue(1, i) == null)
+                    theArray[i - 1] = "";
+                else
+                    theArray[i - 1] = (string)values.GetValue(1, i).ToString();
+            }
+            return theArray;
+        }
+        */
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
